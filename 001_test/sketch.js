24c3031c7f9dccdcf5e811d8d0480f49a3f77556
micuat <micuat@gmail.com>
2018-11-25 11:25:18 +0100

    this ing

--- 001_test/sketch.js
+++ 001_test/sketch.js
@@ -1,37 +1,37 @@
 var S001 = function (p) {
-  let lightPos;
-  let defaultShader;
-  let shadowMap;
-  let name;
-  let lightDirection = {x: 0.0, y: 0.0, z: 0.0};
+  this.lightPos;
+  this.defaultShader;
+  this.shadowMap;
+  this.lightDirection = p.createVector(0, 0, 1);
+  this.name;
 
-  function initShadowPass() {
-    shadowMap = p.createGraphics(2048, 2048, p.P3D);
-    shadowMap.noSmooth(); // Antialiasing on the shadowMap leads to weird artifacts
-    //shadowMap.loadPixels(); // Will interfere with noSmooth() (probably a bug in Processing)
-    shadowMap.beginDraw();
-    shadowMap.noStroke();
-    shadowMap.shader(p.loadShader(name + ("/shadow.frag"), name + ("/shadow.vert")));
-    shadowMap.ortho(-400, 400, -400, 400, -1000, 1000); // Setup orthogonal view matrix for the directional light
-    shadowMap.endDraw();
+  this.initShadowPass = function () {
+    this.shadowMap = p.createGraphics(2048, 2048, p.P3D);
+    this.shadowMap.noSmooth(); // Antialiasing on the shadowMap leads to weird artifacts
+    //this.shadowMap.loadPixels(); // Will interfere with noSmooth() (probably a bug in Processing)
+    this.shadowMap.beginDraw();
+    this.shadowMap.noStroke();
+    this.shadowMap.shader(p.loadShader(this.name + ("/shadow.frag"), this.name + ("/shadow.vert")));
+    this.shadowMap.ortho(-400, 400, -400, 400, -1000, 1000); // Setup orthogonal view matrix for the directional light
+    this.shadowMap.endDraw();
   }
 
-  function initDefaultPass() {
-    defaultShader = p.loadShader(name + ("/default.frag"), name + ("/default.vert"));
-    p.shader(defaultShader);
+  this.initDefaultPass = function () {
+    this.defaultShader = p.loadShader(this.name + ("/default.frag"), this.name + ("/default.vert"));
+    p.shader(this.defaultShader);
     p.noStroke();
     p.perspective(60.0 / 180 * Math.PI, p.width / p.height, 10, 1000);
   }
 
   this.setup = function () {
-    name = p.folderName;
+    this.name = p.folderName;
 
-    initShadowPass();
-    initDefaultPass();
-    lightPos = p.createVector();
+    this.initShadowPass();
+    this.initDefaultPass();
+    this.lightPos = p.createVector();
   }
 
-  function updateDefaultShader() {
+  this.updateDefaultShader = function () {
     // Bias matrix to move homogeneous shadowCoords into the UV texture space
     let shadowTransform = new Packages.processing.core.PMatrix3D(
       0.5, 0.0, 0.0, 0.5,
@@ -41,7 +41,7 @@ var S001 = function (p) {
     );
 
     // Apply project modelview matrix from the shadow pass (light direction)
-    shadowTransform.apply(shadowMap.projmodelview);
+    shadowTransform.apply(this.shadowMap.projmodelview);
 
     // Apply the inverted modelview matrix from the default pass to get the original vertex
     // positions inside the shader. This is needed because Processing is pre-multiplying
@@ -51,7 +51,7 @@ var S001 = function (p) {
 
     // Convert column-minor PMatrix to column-major GLMatrix and send it to the shader.
     // PShader.set(String, PMatrix3D) doesn't convert the matrix for some reason.
-    defaultShader.set("shadowTransform", new Packages.processing.core.PMatrix3D(
+    this.defaultShader.set("shadowTransform", new Packages.processing.core.PMatrix3D(
       shadowTransform.m00, shadowTransform.m10, shadowTransform.m20, shadowTransform.m30,
       shadowTransform.m01, shadowTransform.m11, shadowTransform.m21, shadowTransform.m31,
       shadowTransform.m02, shadowTransform.m12, shadowTransform.m22, shadowTransform.m32,
@@ -60,29 +60,29 @@ var S001 = function (p) {
 
     // Calculate light direction normal, which is the transpose of the inverse of the
     // modelview matrix and send it to the default shader.
-    let lightNormalX = lightPos.x * modelviewInv.m00 + lightPos.y * modelviewInv.m10 + lightPos.z * modelviewInv.m20;
-    let lightNormalY = lightPos.x * modelviewInv.m01 + lightPos.y * modelviewInv.m11 + lightPos.z * modelviewInv.m21;
-    let lightNormalZ = lightPos.x * modelviewInv.m02 + lightPos.y * modelviewInv.m12 + lightPos.z * modelviewInv.m22;
+    let lightNormalX = this.lightPos.x * modelviewInv.m00 + this.lightPos.y * modelviewInv.m10 + this.lightPos.z * modelviewInv.m20;
+    let lightNormalY = this.lightPos.x * modelviewInv.m01 + this.lightPos.y * modelviewInv.m11 + this.lightPos.z * modelviewInv.m21;
+    let lightNormalZ = this.lightPos.x * modelviewInv.m02 + this.lightPos.y * modelviewInv.m12 + this.lightPos.z * modelviewInv.m22;
     let normalLength = Math.sqrt(lightNormalX * lightNormalX + lightNormalY * lightNormalY + lightNormalZ * lightNormalZ);
-    lightDirection = {x: lightNormalX / -normalLength, y: lightNormalY / -normalLength, z: lightNormalZ / -normalLength};
-    defaultShader.set("lightDirection", lightDirection.x, lightDirection.y, lightDirection.z);
-    defaultShader.set("uLightColor", 1.0, 1.0, 1.0);
-    // defaultShader.set("uBaseColor", 0.5, 0.5, 0.5);
+    this.lightDirection.set(lightNormalX / -normalLength, lightNormalY / -normalLength, lightNormalZ / -normalLength);
+    this.defaultShader.set("lightDirection", this.lightDirection.x, this.lightDirection.y, this.lightDirection.z);
+    this.defaultShader.set("uLightColor", 1.0, 1.0, 1.0);
+    // this.defaultShader.set("uBaseColor", 0.5, 0.5, 0.5);
 
-    defaultShader.set("uMetallic", 0.4);
-    defaultShader.set("uRoughness", 0.1);
-    defaultShader.set("uSpecular", 0.99);
-    defaultShader.set("uLightRadius", 500.0);
-    defaultShader.set("uExposure", 2.0);
-    defaultShader.set("uGamma", 0.6);
+    this.defaultShader.set("uMetallic", 0.4);
+    this.defaultShader.set("uRoughness", 0.1);
+    this.defaultShader.set("uSpecular", 0.99);
+    this.defaultShader.set("uLightRadius", 500.0);
+    this.defaultShader.set("uExposure", 2.0);
+    this.defaultShader.set("uGamma", 0.6);
 
-    defaultShader.set("vLightPosition", lightPos.x, lightPos.y, lightPos.z);
+    this.defaultShader.set("vLightPosition", this.lightPos.x, this.lightPos.y, this.lightPos.z);
 
     // Send the shadowmap to the default shader
-    defaultShader.set("shadowMap", shadowMap);
+    this.defaultShader.set("shadowMap", this.shadowMap);
   }
 
-  function renderLandscape(canvas, shader) {
+  this.renderLandscape = function (canvas) {
     canvas.pushMatrix();
 
     canvas.pushMatrix();
@@ -105,12 +105,7 @@ var S001 = function (p) {
   }
 
   this.draw = function () {
-
-    if (p.frameCount % 120 == 0) {
-      // print(p.frameRate());
-      // defaultShader = p.loadShader(name + ("/default.frag"), name + ("/default.vert"));
-      p.shader(defaultShader);
-    }
+    p.shader(this.defaultShader);
 
     let viewMatrix = new Packages.processing.core.PMatrix3D(
       0.5, 0.0, 0.0, 0.5,
@@ -118,16 +113,16 @@ var S001 = function (p) {
       0.0, 0.0, 0.5, 0.5,
       0.0, 0.0, 0.0, 1.0
     );
-    let cameraPosition = {x: 0.0, y: -200.0, z: 150.0};
+    let cameraPosition = p.createVector(0, -200.0, 150.0);
     viewMatrix.translate(cameraPosition.x, cameraPosition.y, cameraPosition.z);
-    defaultShader.set("viewMatrix", new Packages.processing.core.PMatrix3D(
+    this.defaultShader.set("viewMatrix", new Packages.processing.core.PMatrix3D(
       viewMatrix.m00, viewMatrix.m10, viewMatrix.m20, viewMatrix.m30,
       viewMatrix.m01, viewMatrix.m11, viewMatrix.m21, viewMatrix.m31,
       viewMatrix.m02, viewMatrix.m12, viewMatrix.m22, viewMatrix.m32,
       viewMatrix.m03, viewMatrix.m13, viewMatrix.m23, viewMatrix.m33
     ));
     let modelviewInv = p.g.modelviewInv;
-    defaultShader.set("modelviewInv", new Packages.processing.core.PMatrix3D(
+    this.defaultShader.set("modelviewInv", new Packages.processing.core.PMatrix3D(
       modelviewInv.m00, modelviewInv.m10, modelviewInv.m20, modelviewInv.m30,
       modelviewInv.m01, modelviewInv.m11, modelviewInv.m21, modelviewInv.m31,
       modelviewInv.m02, modelviewInv.m12, modelviewInv.m22, modelviewInv.m32,
@@ -138,29 +133,29 @@ var S001 = function (p) {
     p.background(0);
 
     var lightAngle = p.frameCount * 0.02;
-    lightPos.set(10 * Math.cos(lightAngle), -13, 10 * Math.sin(lightAngle));
+    this.lightPos.set(10 * Math.cos(lightAngle), -13, 10 * Math.sin(lightAngle));
 
     // Render shadow pass
-    shadowMap.beginDraw();
-    shadowMap.camera(lightPos.x, lightPos.y, lightPos.z, lightDirection.x-lightPos.x, lightDirection.y-lightPos.y, lightDirection.z-lightPos.z, 0, 1, 0);
-    shadowMap.background(255, 255, 255, 255); // Will set the depth to 1.0 (maximum depth)
-    renderLandscape(shadowMap);
-    shadowMap.endDraw();
+    this.shadowMap.beginDraw();
+    this.shadowMap.camera(this.lightPos.x, this.lightPos.y, this.lightPos.z, this.lightDirection.x-this.lightPos.x, this.lightDirection.y-this.lightPos.y, this.lightDirection.z-this.lightPos.z, 0, 1, 0);
+    this.shadowMap.background(255, 255, 255, 255); // Will set the depth to 1.0 (maximum depth)
+    this.renderLandscape(this.shadowMap);
+    this.shadowMap.endDraw();
     // shadowMap.updatePixels();
 
     // Update the shadow transformation matrix and send it, the light
     // direction normal and the shadow map to the default shader.
-    updateDefaultShader();
+    this.updateDefaultShader();
 
     // Render default pass
     p.background(34, 34, 34, 255);
     p.noStroke();
-    renderLandscape(p.g, defaultShader);
+    this.renderLandscape(p.g, this.defaultShader);
 
     // Render light source
     p.pushMatrix();
     p.fill(255, 255, 255, 255);
-    p.translate(lightPos.x, lightPos.y, lightPos.z);
+    p.translate(this.lightPos.x, this.lightPos.y, this.lightPos.z);
     p.popMatrix();
   }
 
