ce2a0607990ab8def642200d439e5326935c95dd
micuat <micuat@gmail.com>
2018-12-13 20:26:53 +0900

    city

--- 013_cubes/sketch.js
+++ 013_cubes/sketch.js
@@ -5,9 +5,9 @@ function S013 (p) {
   this.texture.beginDraw();
   this.texture.background(255);
   this.texture.endDraw();
-  this.uMetallic = 0.6;
-  this.uRoughness = 0.2;
-  this.uSpecular = 0.99;
+  this.uMetallic = 0.3;
+  this.uRoughness = 0.5;
+  this.uSpecular = 0.9;
   this.uExposure = 3.0;
 
   function Cube (shape, x0, y0, z0, x1, y1, z1, tx0, ty0, tx1, ty1) {
@@ -53,26 +53,28 @@ function S013 (p) {
     shape.vertex(x0, y1, z1, tx1, ty1);
     shape.vertex(x0, y1, z0, tx0, ty1);
   }
-  function Hexagon (shape, x0, y0, z0, r) {
-    for(let i = 0; i < 6; i++) {
-      shape.normal( 0,  -1,  0);
-      shape.vertex(x0, y0, z0, 0, 0);
-      let x1 = x0 + r * Math.cos(i * 2.0 * Math.PI / 6.0);
-      let y1 = y0;
-      let z1 = z0 + r * Math.sin(i * 2.0 * Math.PI / 6.0);
+  function Hexagon (shape, x0, y0, z0, r, h) {
+    let n = 6.0;
+    for(let i = 0; i < n; i++) {
+      shape.normal(0, -1, 0);
+      shape.vertex(x0, y0 - h, z0, 0, 0);
+      let x1 = x0 + r * Math.cos(i * 2.0 * Math.PI / n);
+      let y1 = y0 - h;
+      let z1 = z0 + r * Math.sin(i * 2.0 * Math.PI / n);
       shape.vertex(x1, y1, z1, 0, 0);
-      let x2 = x0 + r * Math.cos((i+1) * 2.0 * Math.PI / 6.0);
-      let y2 = y0;
-      let z2 = z0 + r * Math.sin((i+1) * 2.0 * Math.PI / 6.0);
+      let x2 = x0 + r * Math.cos((i+1) * 2.0 * Math.PI / n);
+      let y2 = y0 - h;
+      let z2 = z0 + r * Math.sin((i+1) * 2.0 * Math.PI / n);
       shape.vertex(x2, y2, z2, 0, 0);
 
+      shape.normal(Math.cos((i + 0.5) * 2.0 * Math.PI / n), 0, Math.sin((i + 0.5) * 2.0 * Math.PI / n));
       shape.vertex(x2, y2, z2, 0, 0);
       shape.vertex(x1, y1, z1, 0, 0);
-      shape.vertex(x1, y0 + 100, z1, 0, 0);
+      shape.vertex(x1, y0 + h, z1, 0, 0);
 
       shape.vertex(x2, y2, z2, 0, 0);
-      shape.vertex(x1, y0 + 100, z1, 0, 0);
-      shape.vertex(x2, y0 + 100, z2, 0, 0);
+      shape.vertex(x1, y0 + h, z1, 0, 0);
+      shape.vertex(x2, y0 + h, z2, 0, 0);
 
     }
   }
@@ -81,9 +83,9 @@ function S013 (p) {
   // Cube(this.shape, -100, -100, -100, 100, 100, 100, 0, 0, 1, 1);
   for(let i = 0; i < 500; i++) {
     let v = p5.Vector.random2D();
-    let radius = p.random(50, 200);
+    let idx = Math.floor(p.random(0, 5));
+    let radius = Math.pow(2, (idx + p.random(1)) + 5);
     v.mult(radius);
-    let idx = Math.floor(p.map(radius, 50, 200, 0, 5));
     let w = Math.pow(2, idx + 2);
     let x = Math.floor(v.x / w) * w;
     // let y = Math.floor(v.y / w) * w;
@@ -94,7 +96,26 @@ function S013 (p) {
     this.shape.noStroke();
     this.shape.fill(this.colorScheme.get(idx).r, this.colorScheme.get(idx).g, this.colorScheme.get(idx).b);
     // Cube(this.shape, x - w/2, y - w/2, z - w/2, x + w/2, y + w/2, z + w/2, 0, 0, 1, 1);
-    Hexagon(this.shape, x, y-50, z, w / Math.sqrt(3) * 0.9);
+    Hexagon(this.shape, x, 50, z, w / Math.sqrt(3) * 0.9, Math.pow(2, 4 - idx + 3));
+    this.shape.endShape(this.p.CLOSE);
+  }
+
+  for(let i = 0; i < 200; i++) {
+    let v = p5.Vector.random2D();
+    let idx = Math.floor(4);
+    let radius = p.random(800);
+    v.mult(radius);
+    let w = Math.pow(2, idx + 2);
+    let x = Math.floor(v.x / w) * w;
+    // let y = Math.floor(v.y / w) * w;
+    let y = -w / 2 + 20 + 40;
+    let z = (Math.floor(v.y / w) + (Math.floor(v.x / w) % 2 == 0 ? 0.5 : 0)) * w;
+    this.shape.beginShape(this.p.TRIANGLES);
+    // this.shape.beginShape(this.p.QUADS);
+    this.shape.noStroke();
+    this.shape.fill(this.colorScheme.get(idx).r, this.colorScheme.get(idx).g, this.colorScheme.get(idx).b);
+    // Cube(this.shape, x - w/2, y - w/2, z - w/2, x + w/2, y + w/2, z + w/2, 0, 0, 1, 1);
+    Hexagon(this.shape, x, 50, z, w / Math.sqrt(3) * 0.9, Math.pow(2, 4 - idx + 3));
     this.shape.endShape(this.p.CLOSE);
   }
 
