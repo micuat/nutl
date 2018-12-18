8d287ed90310587fdb4d1f3bd9cc8bdd9a05f035
micuat <micuat@gmail.com>
2018-12-18 15:52:56 -0500

    fake ambient occlusion test

--- libs/Polygons.js
+++ libs/Polygons.js
@@ -46,24 +46,28 @@ Polygons = {
     let n = 6.0;
     for(let i = 0; i < n; i++) {
       shape.normal(0, -1, 0);
-      shape.vertex(x0, y0 - h, z0, 0, 0);
-      let x1 = x0 + r * Math.cos(i * 2.0 * Math.PI / n);
+      shape.vertex(x0, y0 - h, z0, 0.5, 0.5);
+      let c1 = Math.cos(i * 2.0 * Math.PI / n);
+      let s1 = Math.sin(i * 2.0 * Math.PI / n);
+      let x1 = x0 + r * c1;
       let y1 = y0 - h;
-      let z1 = z0 + r * Math.sin(i * 2.0 * Math.PI / n);
-      shape.vertex(x1, y1, z1, 0, 0);
-      let x2 = x0 + r * Math.cos((i+1) * 2.0 * Math.PI / n);
+      let z1 = z0 + r * s1;
+      shape.vertex(x1, y1, z1, c1 * 0.5 + 0.5, s1 * 0.5 + 0.5);
+      let c2 = Math.cos((i+1) * 2.0 * Math.PI / n);
+      let s2 = Math.sin((i+1) * 2.0 * Math.PI / n);
+      let x2 = x0 + r * c2;
       let y2 = y0 - h;
-      let z2 = z0 + r * Math.sin((i+1) * 2.0 * Math.PI / n);
-      shape.vertex(x2, y2, z2, 0, 0);
+      let z2 = z0 + r * s2;
+      shape.vertex(x2, y2, z2, c2 * 0.5 + 0.5, s2 * 0.5 + 0.5);
 
       shape.normal(Math.cos((i + 0.5) * 2.0 * Math.PI / n), 0, Math.sin((i + 0.5) * 2.0 * Math.PI / n));
-      shape.vertex(x2, y2, z2, 0, 0);
+      shape.vertex(x2, y2, z2, 1, 0);
       shape.vertex(x1, y1, z1, 0, 0);
-      shape.vertex(x1, y0 + h, z1, 0, 0);
+      shape.vertex(x1, y0 + h, z1, 0, 1);
 
-      shape.vertex(x2, y2, z2, 0, 0);
-      shape.vertex(x1, y0 + h, z1, 0, 0);
-      shape.vertex(x2, y0 + h, z2, 0, 0);
+      shape.vertex(x2, y2, z2, 1, 0);
+      shape.vertex(x1, y0 + h, z1, 0, 1);
+      shape.vertex(x2, y0 + h, z2, 1, 1);
 
     }
   }
