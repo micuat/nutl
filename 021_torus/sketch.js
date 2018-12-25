649e8fb0c193bed2f130479e1bc8b4ee5d9b0f42
micuat <micuat@gmail.com>
2018-12-25 16:58:04 -0500

    torus object

--- 021_torus/sketch.js
+++ 021_torus/sketch.js
@@ -52,60 +52,8 @@ SObject.prototype.draw = function (that, pg) {
   let p = that.p;
 }
 
-function SSphere (p) {
+function STorus (p) {
   SObject.call(this, p);
-  this.shape = p.createShape(p.GROUP);
-  let n = 128;
-  let r = 100;
-  for(let i = -n; i <= n; i++) {
-    let s = p.createShape();
-    s.beginShape(this.p.TRIANGLE_STRIP);
-    s.noStroke();
-    s.fill(255);
-    for(let j = -n; j <= n; j++) {
-      for(let ii = 1; ii >= 0; ii--) {
-        let theta = p.map(i + ii, -n, n, -Math.PI, Math.PI);
-        let phi = p.map(j, -n, n, 0, Math.PI);
-        let x0 = r * Math.sin(phi) * Math.cos(theta);
-        let y0 = r * Math.sin(phi) * Math.sin(theta);
-        let z0 = r * Math.cos(phi);
-        s.normal(x0, y0, z0);
-        s.vertex(x0, y0, z0, phi / Math.PI, (theta / Math.PI) * 0.5 + 0.5);
-      }
-    }
-    s.endShape(this.p.CLOSE);
-    this.shape.addChild(s);
-  }
-}
-
-SSphere.prototype = Object.create(SObject.prototype, {
-  draw: {
-    value: function (pg) {
-      let p = this.p;
-      let scale = EasingFunctions.easeInOutCubic(this.fadeIn * (1.0 - this.fadeOut));
-      pg.pushMatrix();
-      pg.scale(scale, scale, scale);
-      pg.shape(this.shape, 0, 0);
-      pg.popMatrix();
-      Object.getPrototypeOf(SSphere.prototype).draw(this);
-    }
-  }
-});
-
-SSphere.prototype.constructor = SSphere;
-
-function S021 (p, w, h) {
-  SRendererShadow.call(this, p, w, h);
-  this.colorScheme = new ColorScheme("8a6552-462521-ca2e55-dde0b5-bdb246");
-  this.texture = p.createGraphics(40, 40, p.P3D);
-  this.texture.beginDraw();
-  this.texture.background(255);
-  this.texture.endDraw();
-  this.uMetallic = 0.3;
-  this.uRoughness = 0.5;
-  this.uSpecular = 0.9;
-  this.uExposure = 4.0;
-  this.uVignette = 0.0;
 
   this.pts = 40; 
   this.angle = 0;
@@ -125,6 +73,96 @@ function S021 (p, w, h) {
   // for optional helix
   this.isHelix = false;
   this.helixOffset = 5.0;
+  
+  this.shape = p.createShape(p.GROUP);
+
+  // initialize point arrays
+  this.vertices = [];
+  this.vertices2 = [];
+
+  // fill arrays
+  for(let i=0; i<=this.pts; i++){
+    this.vertices[i] = p.createVector();
+    this.vertices2[i] = p.createVector();
+    this.vertices[i].x = this.latheRadius + Math.sin(p.radians(this.angle))*this.radius;
+    if (this.isHelix){
+      this.vertices[i].z = Math.cos(p.radians(this.angle))*this.radius-(this.helixOffset* 
+        this.segments)/2;
+    } 
+    else{
+      this.vertices[i].z = Math.cos(p.radians(this.angle))*this.radius;
+    }
+    this.angle+=360.0/this.pts;
+  }
+  
+  // draw toroid
+  this.latheAngle = 0;
+  for(let i=0; i<=this.segments; i++) {
+    let s = p.createShape();
+    s.beginShape(p.QUAD_STRIP);
+    if (this.isWireFrame){
+      s.stroke(255, 255, 150);
+      s.noFill();
+    }
+    else {
+      s.noStroke();
+      s.fill(200);
+    }
+    for(let j=this.pts; j>=0; j--){
+      if (i>0){
+        s.vertex(this.vertices2[j].x, this.vertices2[j].y, this.vertices2[j].z);
+      }
+      this.vertices2[j].x = Math.cos(p.radians(this.latheAngle))*this.vertices[j].x;
+      this.vertices2[j].y = Math.sin(p.radians(this.latheAngle))*this.vertices[j].x;
+      this.vertices2[j].z = this.vertices[j].z;
+      // optional helix offset
+      if (this.isHelix){
+        this.vertices[j].z+=this.helixOffset;
+      } 
+      s.vertex(this.vertices2[j].x, this.vertices2[j].y, this.vertices2[j].z);
+    }
+    // create extra rotation for helix
+    if (this.isHelix){
+      this.latheAngle+=720.0/this.segments;
+    } 
+    else {
+      this.latheAngle+=360.0/this.segments;
+    }
+    s.endShape();
+    this.shape.addChild(s);
+  }
+}
+
+STorus.prototype = Object.create(SObject.prototype, {
+  draw: {
+    value: function (pg) {
+      let p = this.p;
+      let scale = EasingFunctions.easeInOutCubic(this.fadeIn * (1.0 - this.fadeOut));
+      pg.pushMatrix();
+      // pg.scale(scale, scale, scale);
+      pg.shape(this.shape, 0, 0);
+      pg.popMatrix();
+      Object.getPrototypeOf(STorus.prototype).draw(this);
+    }
+  }
+});
+
+STorus.prototype.constructor = STorus;
+
+function S021 (p, w, h) {
+  SRendererShadow.call(this, p, w, h);
+  this.colorScheme = new ColorScheme("8a6552-462521-ca2e55-dde0b5-bdb246");
+  this.texture = p.createGraphics(40, 40, p.P3D);
+  this.texture.beginDraw();
+  this.texture.background(255);
+  this.texture.endDraw();
+  this.uMetallic = 0.3;
+  this.uRoughness = 0.5;
+  this.uSpecular = 0.9;
+  this.uExposure = 4.0;
+  this.uVignette = 0.0;
+
+  this.shape = new STorus(p);
 }
 
 S021.prototype = Object.create(SRendererShadow.prototype, {
@@ -133,61 +171,11 @@ S021.prototype = Object.create(SRendererShadow.prototype, {
       let p = this.p;
       pg.clear();
       pg.pushMatrix();
-      if(!isShadow) {
-        let idx = 4;
-        // pg.background(this.colorScheme.get(idx).r, this.colorScheme.get(idx).g, this.colorScheme.get(idx).b);
-      }
-      //center and spin toroid
-      // pg.translate(pg.width/2, pg.height/2, -100);
     
       pg.rotateX(p.frameCount*Math.PI/150);
       pg.rotateY(p.frameCount*Math.PI/170);
       pg.rotateZ(p.frameCount*Math.PI/90);
-
-      pg.translate(0, 0, p.map(Math.sin(p.frameCount / 60.0 * Math.PI), -1, 1, 0, -500));
-      
-      // draw toroid
-      this.latheAngle = 0;
-      for(let i=0; i<=this.segments; i++){
-        if (this.isWireFrame){
-          pg.stroke(255, 255, 150);
-          pg.noFill();
-        } 
-        else {
-          pg.noStroke();
-          if(i % 2 == 0) {
-            let idx = 2;
-            pg.fill(this.colorScheme.get(idx).r, this.colorScheme.get(idx).g, this.colorScheme.get(idx).b);
-          }
-          else {
-            pg.fill(200);
-          }
-        }
-        pg.beginShape(p.QUAD_STRIP);
-        for(let j=this.pts; j>=0; j--){
-          if (i>0){
-            pg.vertex(this.vertices2[j].x, this.vertices2[j].y, this.vertices2[j].z);
-          }
-          this.vertices2[j].x = Math.cos(p.radians(this.latheAngle))*this.vertices[j].x;
-          this.vertices2[j].y = Math.sin(p.radians(this.latheAngle))*this.vertices[j].x;
-          this.vertices2[j].z = this.vertices[j].z;
-          // optional helix offset
-          // if (this.isHelix){
-          //   this.vertices[j].z+=this.helixOffset;
-          // } 
-          this.vertices[j].z+=p.map(Math.sin(p.frameCount / 60.0 * Math.PI), -1, 1, 0, this.helixOffset);
-          pg.vertex(this.vertices2[j].x, this.vertices2[j].y, this.vertices2[j].z);
-        }
-        // create extra rotation for helix
-        // if (this.isHelix){
-        //   this.latheAngle+=720.0/this.segments;
-        // } 
-        // else {
-        //   this.latheAngle+=360.0/this.segments;
-        // }
-        this.latheAngle+=p.map(Math.sin(p.frameCount / 60.0 * Math.PI), -1, 1, 360, 720)/this.segments;
-        pg.endShape();
-      }
+      this.shape.draw(pg);
       pg.popMatrix();
 
       let idx = 4;
@@ -204,24 +192,6 @@ S021.prototype = Object.create(SRendererShadow.prototype, {
       this.lightPos.set(100, -300, 200);
       this.lightDirection = this.lightPos;
 
-      // initialize point arrays
-      this.vertices = [];
-      this.vertices2 = [];
-    
-      // fill arrays
-      for(let i=0; i<=this.pts; i++){
-        this.vertices[i] = p.createVector();
-        this.vertices2[i] = p.createVector();
-        this.vertices[i].x = this.latheRadius + Math.sin(p.radians(this.angle))*this.radius;
-        if (this.isHelix){
-          this.vertices[i].z = Math.cos(p.radians(this.angle))*this.radius-(this.helixOffset* 
-            this.segments)/2;
-        } 
-        else{
-          this.vertices[i].z = Math.cos(p.radians(this.angle))*this.radius;
-        }
-        this.angle+=360.0/this.pts;
-      }
       Object.getPrototypeOf(S021.prototype).draw(this);
     }
   }
