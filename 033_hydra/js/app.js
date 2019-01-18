b8d033ad34a7e09566f1855adaab797c23b58758
micuat <micuat@gmail.com>
2019-01-18 13:01:35 +0100

    033 refactor

--- 033_hydra/js/app.js
+++ 033_hydra/js/app.js
@@ -2,18 +2,290 @@ const Hydra = require('hydra-synth')
 
 
 window.onload = function () {
-  let canvas = document.createElement('canvas');
-  canvas.setAttribute("id", "hydra-canvas0-tex");
-  canvas.style.width = "1024px"
-  canvas.style.height = "1024px"
-  canvas.width = 1024;
-  canvas.height = 1024;
-  document.body.appendChild(canvas)
-  canvas.style.display = "none";
+  let canvas;
+  function setupHydra() {
+    canvas = document.createElement('canvas');
+    canvas.setAttribute("id", "hydra-canvas0-tex");
+    canvas.style.width = "1024px"
+    canvas.style.height = "1024px"
+    canvas.width = 1024;
+    canvas.height = 1024;
+    document.body.appendChild(canvas)
+    canvas.style.display = "none";
 
-  const hydra = new Hydra({canvas: canvas})
+    const hydra = new Hydra({ canvas: canvas })
 
-  // by default, hydra makes everything global.
-  // see options to change parameters
-  hydra.osc().color(0.1).pixelate(200,200).kaleid(2).out()
+    // by default, hydra makes everything global.
+    // see options to change parameters
+    // hydra.osc(() => (100 * Math.sin(time * 0.1))).color(0.1).modulate(noise()).kaleid(2).out();
+    hydra.noise(2,0.5).color(1,0,4).colorama(0.001).scale(0.25).rotate(0, [0, Math.PI*2]).out(o1)
+    hydra.src(o1).add(o0,0.95).scale(1.01).out(o0)
+  }
+  setupHydra();
+
+  let light1, light2;
+  let renderer, scene, camera, cube;
+  let uniforms = {};
+  let comp;
+  let spreadTubes = 2;
+  let meshes = [];
+
+  function setupFlocking() {
+    fluid.registerNamespace("testugen");
+
+    testugen.click = function (inputs, output, options) {
+      var that = flock.ugen(inputs, output, options);
+      console.log(that.inputs)
+
+      that.gen = function (numSamps) {
+        var out = that.output,
+          m = that.model,
+          i;
+
+        for (i = 0; i < numSamps; i++) {
+          out[i] = m.unscaledValue;
+        }
+
+        that.mulAdd(numSamps);
+        m.value = flock.ugen.lastOutputValue(numSamps, out);
+      };
+
+      that.init = function () {
+        setInterval(() => {
+          that.model.unscaledValue = 1.0;
+          setTimeout(() => {
+            that.model.unscaledValue = 0.0;
+          }, 100);
+        }, 1000);
+        that.onInputChanged();
+      };
+
+      that.onInputChanged = function () {
+        flock.onMulAddInputChanged(that);
+      };
+
+      that.init();
+      return that;
+    };
+    flock.ugenDefaults("testugen.click", {
+      rate: "audio",
+      func: undefined
+    })
+
+    // Define an Infusion component that represents your instrument.
+    fluid.defaults("myStuff.sinewaver",
+      {
+        gradeNames: ["flock.synth"],
+
+        // Define the synthDef for your instrument.
+        synthDef: {
+          id: "parent",
+          ugen: "flock.ugen.squareOsc",
+          freq:
+          {
+            ugen: "flock.ugen.triggerCallback",
+            source: {
+              ugen: "flock.ugen.sequencer",
+              id: "sqnotes",
+              durations: [0.25, 0.25, 0.25, 0.25],
+              values: [1, 10 / 8, 3 / 4, 3 / 2],
+              mul: 55 / 2,
+              loop: 1.0,
+              options: {
+                holdLastValue: true
+              }
+            },
+            trigger: {
+              ugen: "flock.ugen.impulse",
+              freq: 60
+            },
+            options: {
+              callback: {
+                func: (v) => {
+                  if (v / (55 / 2) < 1) {
+                    uniforms.uColor.value.x = 219.0 / 255;
+                    uniforms.uColor.value.y = 39.0 / 255;
+                    uniforms.uColor.value.z = 99.0 / 255;
+                  }
+                  else {
+                    uniforms.uColor.value.x = 18.0 / 255;
+                    uniforms.uColor.value.y = 234.0 / 255;
+                    uniforms.uColor.value.z = 234.0 / 255;
+                  }
+                  //spreadTubes = v * 0.05 * 0.05 + 0.95 * spreadTubes;
+                },
+                args: []
+              }
+            }
+          },
+          mul: {
+            ugen: "flock.ugen.triggerCallback",
+            source: {
+              ugen: "flock.ugen.envGen",
+              envelope: {
+                levels: [0, 1, 0],
+                times: [0.03, 0.1]
+              },
+              gate: {
+                ugen: "flock.ugen.sequencer",
+                durations: [0.25, 0.25, 0.2, 0.2],//, 0.3, 0.25, 0.25],
+                values: [1, 1, 1, 1],//, 1, 1, 1],
+                loop: 1.0,
+                options: {
+                  resetOnNext: true
+                }
+              },
+              mul: 0
+            },
+            trigger: {
+              ugen: "flock.ugen.impulse",
+              freq: 60
+            },
+            options: {
+              callback: {
+                func: (v) => {
+                  uniforms.delta.value += 1 * v + 0.1;
+                },
+                args: []
+              }
+            }
+          }
+        }
+      }
+    );
+
+    fluid.defaults("myStuff.composition", {
+      gradeNames: ["fluid.component"],
+      components: {
+        environment: {
+          type: "flock.enviro"
+        },
+
+        instrument: {
+          type: "myStuff.sinewaver"
+        }
+      },
+      listeners: {
+        "onCreate.startEnvironment": {
+          func: "{environment}.start"
+        }
+      }
+    });
+    comp = myStuff.composition();
+  }
+
+  function setupThreejs() {
+    scene = new THREE.Scene();
+
+    // camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera = new THREE.PerspectiveCamera(75, 1.0, 0.1, 1000);
+    camera.position.set(0, 0, 15);
+    camera.lookAt(0, 0, 0);
+
+    let sphere = new THREE.SphereBufferGeometry(0.5, 16, 8);
+
+    light1 = new THREE.PointLight(0xffffff, 2, 50);
+    scene.add(light1);
+
+    light2 = new THREE.PointLight(0xffffff, 2, 50);
+    light2.position.set(0, -5, 15);
+    scene.add(light2);
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(1024, 1024);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    document.body.appendChild(renderer.domElement);
+
+    // reuse the fragment shader stuff from the lambert material
+    let basicShader = THREE.ShaderLib['standard'];
+    uniforms = basicShader.uniforms;
+    uniforms.uColor = { value: new THREE.Vector3() };
+    uniforms.delta = { type: 'f', value: 0.0 };
+    uniforms.scale = { type: 'f', value: 0.5 };
+    uniforms.time = {type: 'f', value: 0.0};
+
+    var defines = {};
+    defines["USE_MAP"] = "";
+    let sinusMaterial = new THREE.ShaderMaterial({
+      defines: defines,
+      uniforms: uniforms,
+      vertexShader: document.
+        getElementById('sinusVertexShader').text,
+      fragmentShader: basicShader.fragmentShader,
+      lights: true,
+      // vertexColors: THREE.VertexColors,
+      fog: true
+    });
+
+    // create a cube and add to scene
+    cube = new THREE.Mesh(new THREE.SphereGeometry(7, 150, 150), sinusMaterial);
+    // cube.name = 'cube';
+    scene.add(cube);
+
+    class CustomSinCurve extends THREE.Curve {
+      constructor(scale = 1) {
+        super();
+        this.scale = scale;
+      }
+      getPoint(t) {
+        let tx = t * 3 - 1.5;
+        let ty = Math.sin(2 * Math.PI * t);
+        let tz = Math.cos(2 * Math.PI * t);
+
+        return new THREE.Vector3(tx, ty, tz).multiplyScalar(this.scale);
+      }
+    }
+
+    let path = new CustomSinCurve(10);
+    let geometry = new THREE.TubeGeometry(path, 200, 0.2, 8, false);
+
+    let material = new THREE.MeshStandardMaterial({ color: 0x999999 });
+    let n = 2;
+    for (let i = -n; i <= n; i++) {
+      for (let j = -n; j <= n; j++) {
+        let mesh = new THREE.Mesh(geometry, material);
+        mesh.position.x = j * 5;
+        mesh.position.y = i * 5;
+        mesh.position.z = 0;
+        mesh.rotation.y = Math.sin(i * 0.02);
+        mesh.rotation.x = Math.sin(j * 0.02);
+        mesh.matrixAutoUpdate = false;
+        mesh.updateMatrix();
+        scene.add(mesh);
+        meshes.push({ mesh: mesh, i: i, j: j });
+      }
+    }
+  }
+  setupThreejs();
+  setupFlocking();
+  let hydraTexture;
+  function animate() {
+    uniforms['time'].value += 0.01;
+
+    if (uniforms['map'].value == null) {
+      hydraTexture = new THREE.Texture(document.getElementById("hydra-canvas0-tex"));
+      if (document.getElementById("hydra-canvas0-tex") != null) {
+        // scene.background = hydraTexture;
+        uniforms['map'].value = hydraTexture;
+      }
+    }
+    let time = Date.now() * 0.0005;
+    light1.position.x = Math.sin(time * 0.7) * 10;
+    light1.position.y = Math.cos(time * 0.5) * 20;
+    light1.position.z = Math.cos(time * 0.3) * 10;
+
+    // light2.position.x = Math.sin(time * 0.3) * 10;
+    // light2.position.y = Math.cos(time * 0.5) * 20;
+    // light2.position.z = Math.cos(time * 0.7) * 10;
+
+    for (let m of meshes) {
+      m.mesh.position.x = m.j * 5 * spreadTubes;
+      m.mesh.position.y = m.i * 5 * spreadTubes;
+      m.mesh.updateMatrix();
+    }
+    requestAnimationFrame(animate);
+    hydraTexture.needsUpdate = true;
+    renderer.render(scene, camera);
+  }
+  animate();
 }
--- 033_hydra/webpack.config.js
+++ 033_hydra/webpack.config.js
@@ -3,7 +3,8 @@ const path = require('path');
 module.exports = {
   entry: './js/app.js',
   output: {
-    filename: './js/bundle.js',
+    filename: './bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  mode: 'development'
 };
