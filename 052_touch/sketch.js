d2a190f7c92a55726875ef30b8ef41ef4e5a75d7
micuat <micuat@gmail.com>
2019-03-19 22:43:33 +0100

    052 testing direction

--- 052_touch/sketch.js
+++ 052_touch/sketch.js
@@ -7,6 +7,7 @@ var Tile = function (args) {
   this.idy = args.idy;
   this.dx = args.dx == undefined ? 0 : args.dx;
   this.dy = args.dy == undefined ? 0 : args.dy;
+  this.direction = args.direction == undefined ? new p5.Vector(0, 0) : args.direction;
   this.tick = args.tick;
   this.size = this.tick * 0.7;
   this.connectorSize = this.tick - this.size;
@@ -20,7 +21,7 @@ Tile.prototype.draw = function (args) {
   let pg = args.pg;
   pg.push();
   let px = this.idx * this.tick * (hexMode ? 1 / 2 * Math.sqrt(3) : 1);
-  let py = this.idy * this.tick + ((hexMode && this.idx % 2 == 1) ? this.tick / 2 : 0)
+  let py = this.idy * this.tick + ((hexMode && this.idx % 2 == 1) ? -this.tick / 2 : 0)
   pg.translate(px, py, 0);
   let c0 = (this.colorIndex + args.colorShift) % 5;
   let tw0 = EasingFunctions.easeInOutCubic(Math.min(1, tween * 2));
@@ -37,8 +38,15 @@ Tile.prototype.draw = function (args) {
   }
   pg.push();
   pg.fill(255);
-  if(hexMode && this.dx != 0) {
-    pg.rotate(-Math.PI/6);
+  if(hexMode) {
+    if(this.dx != 0) {
+      if(this.idx % 2 == 1) {
+        pg.rotate(-Math.PI/6);
+      }
+      if(this.idx % 2 == 0) {
+        pg.rotate(Math.PI/6);
+      }
+    }
   }
   pg.translate(this.connectorSize * (1 - tw0) * -this.dx, this.connectorSize * (1 - tw0) * -this.dy);
   pg.translate(this.tick * -this.dx / 2, this.tick * -this.dy / 2, -0.001);
@@ -51,7 +59,7 @@ Tile.prototype.draw = function (args) {
     let n = 6;
     pg.beginShape();
     for(let i = 0; i < n; i++) {
-      let r = this.size * 0.7;
+      let r = this.size / 0.7 * 0.5;
       let x = r * Math.cos((i+0.5) / n * 2 * Math.PI);
       let y = r * Math.sin((i+0.5) / n * 2 * Math.PI);
       pg.vertex(x, y);
@@ -78,6 +86,9 @@ Tile.prototype.draw = function (args) {
     }
   }
 
+  // pg.stroke(255);
+  // pg.line(0, 0, this.direction.x, this.direction.y);
+
   pg.pop();
 }
 
@@ -86,7 +97,7 @@ Tile.prototype.trigger = function (args) {
   let correcty = args.y;// + this.unitSize * y;
 
   let px = this.idx * this.tick * (hexMode ? 1 / 2 * Math.sqrt(3) : 1);
-  let py = this.idy * this.tick + ((hexMode && this.idx % 2 == 1) ? this.tick / 2 : 0)
+  let py = this.idy * this.tick + ((hexMode && this.idx % 2 == 1) ? -this.tick / 2 : 0)
 
   if(Math.abs(px - correctx) > this.size / 2 || Math.abs(py - correcty) > this.size / 2) {
     return;
@@ -98,6 +109,10 @@ Tile.prototype.trigger = function (args) {
   let tiles = args.tiles;
   let dx = 0;
   let dy = 0;
+  let direction = new p5.Vector(centricx, centricy);
+  let angle = direction.heading();
+  angle = ((Math.floor(angle * 6 / (Math.PI*2))) / 6 * 2 * Math.PI);
+  direction.set(this.size * Math.cos(angle), this.size * Math.sin(angle));
   let colorIndex = 0;
   if(Math.abs(centricx) > Math.abs(centricy)) {
     dx = centricx > 0 ? 1 : -1;
@@ -112,12 +127,13 @@ Tile.prototype.trigger = function (args) {
     colorIndex = 4;
   }
 
-  if(tiles[this.idy + dy][this.idx + dx] == undefined) {
+  if(tiles[this.idy + dy] != undefined && tiles[this.idy + dy][this.idx + dx] == undefined) {
     tiles[this.idy + dy][this.idx + dx] = new Tile({
       idx: this.idx + dx,
       idy: this.idy + dy,
       dx: dx,
       dy: dy,
+      direction: direction,
       tick: this.tick,
       startTime: args.t,
       colorIndex: colorIndex,
