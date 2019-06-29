d778e99dd9cdd0220d877890c316583e2635f700
micuat <micuat@gmail.com>
2019-06-29 12:57:46 +0200

    066 threading

--- 066_weave/sketch.js
+++ 066_weave/sketch.js
@@ -20,79 +20,87 @@ function S066(p, w, h) {
   this.image = p.loadImage(p.folderName + "/map.png");
 
   this.matrix = [];
-  for(let i = 0; i < this.I + 1; i++) { // oh...
-    this.matrix[i] = [];
-    for(let j = 0; j < this.J; j++) {
-      let dj = i % 2 == 0 ? 0 : 0.5;
-      let x = (j+dj) * this.W;
-      let y = i * this.H;
+  this.calcDone = false;
+  let self = this;
+  var Thread = Java.type('java.lang.Thread');
+  new Thread(function () {
+    for(let i = 0; i < self.I + 1; i++) { // oh...
+      self.matrix[i] = [];
+      for(let j = 0; j < self.J; j++) {
+        let dj = i % 2 == 0 ? 0 : 0.5;
+        let x = (j+dj) * self.W;
+        let y = i * self.H;
 
-      let H = 0;
-      let col = 1;//i % 4;
-      H = 0.25;
-      this.matrix[i][j] = {H: H, col: col, x: x, y: y, cut: false};
+        let H = 0;
+        let col = 0;//i % 4;
+        H = 0.25;
+        self.matrix[i][j] = {H: H, col: col, x: x, y: y, j: j, i: i, cut: false};
+      }
     }
-  }
-  let prevOne = undefined;
-  for(let i = 0; i < this.I + 1; i++) { // oh...
-    function assign(self, i, j) {
-      if(prevOne == undefined) {
-        prevOne = self.matrix[i][j];
+    let prevOne = undefined;
+    for(let i = 0; i < self.I + 1; i++) { // oh...
+      function assign(self, i, j) {
+        if(prevOne == undefined) {
+          prevOne = self.matrix[i][j];
+        }
+        else {
+          self.matrix[i][j].prev = prevOne;
+          prevOne = self.matrix[i][j];
+        }
+        if(prevOne.prev != undefined) {
+          prevOne.prev.next = prevOne;
+        }
+      }
+      if(i % 2 == 1) {
+        for(let j = 0; j < self.J; j++) {
+          assign(self, i, j);
+        }
       }
       else {
-        self.matrix[i][j].prev = prevOne;
-        prevOne = self.matrix[i][j];
-      }
-      if(prevOne.prev != undefined) {
-        prevOne.prev.next = prevOne;
+        for(let j = self.J - 1; j >= 0; j--) {
+          assign(self, i, j);
+        }
       }
     }
-    if(i % 2 == 1) {
-      for(let j = 0; j < this.J; j++) {
-        assign(this, i, j);
-      }
-    }
-    else {
-      for(let j = this.J - 1; j >= 0; j--) {
-        assign(this, i, j);
-      }
-    }
-  }
-  for(let i = 0; i < this.I + 1; i++) { // oh...
-    for(let j = 1; j < this.J; j++) {
+    prevOne = self.matrix[0][0];
+    do {
+      let j = prevOne.j;
+      let i = prevOne.i;
       let dj = i % 2 == 0 ? 0 : 0.5;
-      let b = p.brightness(this.image.get((j+dj)/(this.J+1)*this.image.width/1.5, i/this.I*this.image.height/1.5)) / 255;
-      if((i/4+j)%2 < 0.5 && b > 0.5) {
-        this.matrix[i][j].cut = true;
+      let b = p.brightness(self.image.get((j+dj)/(self.J+1)*self.image.width/1.5, i/self.I*self.image.height/1.5)) / 255;
+      if(b > 0.5) {
+        self.matrix[i][j].cut = true;
         let h = 0;
         for(let n = 0; n < 100; n++) {
-          let b = p.brightness(this.image.get(
-            (j+dj)/(this.J+1)*this.image.width/1.5,
-            (i+n)/this.I*this.image.height/1.5
+          let b = p.brightness(self.image.get(
+            (j+dj)/(self.J+1)*self.image.width/1.5,
+            (i+n)/self.I*self.image.height/1.5
           )) / 255;
           if(b > 0.5) h+=0.1;
           else break;
         }
-        this.matrix[i][j].H = h;
-        let bl = p.brightness(this.image.get((j+dj-1)/(this.J+1)*this.image.width/1.5, i/this.I*this.image.height/1.5)) / 255;
+        self.matrix[i][j].H = Math.max(2, h);
+        let bl = p.brightness(self.image.get((j+dj-1)/(self.J+1)*self.image.width/1.5, i/self.I*self.image.height/1.5)) / 255;
         if(bl < 0.5) {
-          let prev = this.matrix[i][j];
+          let prev = self.matrix[i][j];
           do {
             prev.col = 3;
             prev = prev.prev;
           } while(prev != undefined && prev.cut != true);
         }
-        let br = p.brightness(this.image.get((j+dj+1)/(this.J+1)*this.image.width/1.5, i/this.I*this.image.height/1.5)) / 255;
+        let br = p.brightness(self.image.get((j+dj+1)/(self.J+1)*self.image.width/1.5, i/self.I*self.image.height/1.5)) / 255;
         if(br < 0.5) {
-          let prev = this.matrix[i][j];
+          let prev = self.matrix[i][j];
           do {
-            prev.col = 0;
+            prev.col = 1;
             prev = prev.prev;
           } while(prev != undefined && prev.cut != true);
         }
       }
-    }
-  }
+      prevOne = prevOne.next;
+    } while(prevOne != undefined);
+    self.calcDone = true;
+  }).start();
 }
 
 S066.prototype = Object.create(TLayer.prototype);
@@ -111,6 +119,8 @@ S066.prototype.drawLayer = function(pg, key, args) {
   // setColor(pg, "background", 3);
   pg.background(255);
 
+  pg.translate(280, 0);
+
   pg.pushMatrix();
 
   let W = this.W;
@@ -120,7 +130,8 @@ S066.prototype.drawLayer = function(pg, key, args) {
   pg.strokeWeight(2);
   pg.noFill();
 
-  function drawCurve(args) {
+  pg.translate(30, 0);
+  function drawCurve(args, overrideAlpha) {
     setColor(pg, "stroke", args.col, 150);
     if(args.prev != undefined && args.cut == false) {
       pg.beginShape();
@@ -133,7 +144,7 @@ S066.prototype.drawLayer = function(pg, key, args) {
         let alpha = p.constrain(p.map(y, 0, 0.7, 50, 255), 0, 255);
         if(args.H == 0) alpha = 50;
         // alpha = 150;
-        setColor(pg, "stroke", args.col, alpha);
+        setColor(pg, "stroke", args.col, alpha * overrideAlpha);
         pg.vertex(X, y * hh + Y);
       }
       pg.endShape();
@@ -150,7 +161,7 @@ S066.prototype.drawLayer = function(pg, key, args) {
         let alpha = p.constrain(p.map(y, 0, 0.7, 50, 255), 0, 255);
         if(args.H == 0) alpha = 50;
         // alpha = 150;
-        setColor(pg, "stroke", args.col, alpha);
+        setColor(pg, "stroke", args.col, alpha * overrideAlpha);
         pg.vertex(X, y * hh + Y);
       }
       pg.endShape();
@@ -165,17 +176,17 @@ S066.prototype.drawLayer = function(pg, key, args) {
         let alpha = p.constrain(p.map(y, 0, 0.7, 50, 255), 0, 255);
         if(args.H == 0) alpha = 50;
         // alpha = 150;
-        setColor(pg, "stroke", args.next.col, alpha);
+        setColor(pg, "stroke", args.next.col, alpha * overrideAlpha);
         pg.vertex(X, y * hh + Y);
       }
       pg.endShape();
     }
   }
-  function drawWeft(matrix, i, debug) {
+  function drawWeft(matrix, i, overrideAlpha, debug) {
     for(let j = 0; j < J; j++) {
       let dj = i % 2 == 0 ? 0 : 0.5;
       pg.push();
-      drawCurve(matrix[i][j]);
+      drawCurve(matrix[i][j], overrideAlpha);
 
       if(debug) {
         pg.fill(0)
@@ -188,16 +199,17 @@ S066.prototype.drawLayer = function(pg, key, args) {
   for(let i = 0; i < this.I; i++) {
     pg.push();
     pg.translate(0, this.matrix[i][0].y);
-    drawWeft(this.matrix, i);
+    drawWeft(this.matrix, i, 0.3);
     pg.pop();
   }
 
   pg.push();
   pg.translate(0, 950);
-  let line = Math.floor(p.map(p.mouseX, 0, p.width, 0, 159));//159;
-  drawWeft(this.matrix, line,true);
-  pg.translate(0, 100);
-  drawWeft(this.matrix, line+1,true);
+  // let line = Math.floor(p.map(p.mouseX, 0, p.width, 0, 159));//159;
+  let line = 10;
+  drawWeft(this.matrix, line, 1, true);
+  pg.translate(0, 200);
+  drawWeft(this.matrix, line+1, 1, true);
   pg.pop();
 
   pg.popMatrix();
@@ -208,13 +220,12 @@ S066.prototype.constructor = S066;
 ////////
 
 var s = function (p) {
-  let s066 = new S066(p, 1200, 1200);
+  let s066 = new S066(p, 1500, 1500);
 
   p.setup = function () {
-    p.createCanvas(1200, 1200);
+    p.createCanvas(1500, 1500);
     p.frameRate(60);
     let t = p.millis() * 0.001;
-    s066.draw({t: t});
   }
 
   p.draw = function () {
@@ -226,7 +237,11 @@ var s = function (p) {
 
     p.background(0);
 
-    s066.draw({t: t});
+    if(s066.calcDone == true) {
+      s066.draw({t: t});
+      s066.calcDone = false;
+    }
+    // s066.draw({t: t});
     p.image(s066.pg, 0, 0);
   }
 };
